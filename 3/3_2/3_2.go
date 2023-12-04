ff9f4d4d1082b22fd7241240de48e9dce4a4fe92
daronwastaken <zwierzchowski.kacper1@gmail.com>
2023-12-04 03:45:41 +0100

    Day 3 part 2 done legit
    Refactor: I honestly have no idea how, but somehow managed to do it

--- 3/3_2/3_2.go
+++ 3/3_2/3_2.go
@@ -11,70 +11,187 @@ import (
 
 const FilePath = "../input.txt"
 
-func main() {
-	// Read the file into a 2D slice
-	grid := readGrid(FilePath)
-
-	// Find gears and calculate their gear ratios
-	var sumGearRatio int
-	for y, row := range grid {
-		for x, char := range row {
-			if char == '*' {
-				gearRatio := calculateGearRatio(grid, x, y)
-				sumGearRatio += gearRatio
-			}
-		}
-	}
-
-	fmt.Println("Sum of gear ratios:", sumGearRatio)
+type Number struct {
+	x     int
+	y     int
+	value int
 }
 
-// Reads the file and returns a 2D slice of runes
-func readGrid(filePath string) [][]rune {
-	file, err := os.Open(filePath)
+func main() {
+	// Open the file
+	file, err := os.Open(FilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	var grid [][]rune
+	// Create a 2D slice (2D char array)
+	var charArray [][]rune
+
+	// Read the file line by line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		grid = append(grid, []rune(scanner.Text()))
+		var lineSlice []rune
+		for _, ch := range scanner.Text() {
+			lineSlice = append(lineSlice, ch)
+		}
+		charArray = append(charArray, lineSlice)
 	}
-	return grid
-}
 
-// Calculates the gear ratio for a gear at a given position
-func calculateGearRatio(grid [][]rune, x, y int) int {
-	var numbers []int
-	directions := [][2]int{{0, -1}, {0, 1}, {-1, 0}, {1, 0}} // Left, Right, Up, Down
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-	for _, dir := range directions {
-		nx, ny := x+dir[1], y+dir[0]
-		if nx >= 0 && nx < len(grid[0]) && ny >= 0 && ny < len(grid) && unicode.IsDigit(grid[ny][nx]) {
-			number := extractNumber(grid, nx, ny, dir)
-			numbers = append(numbers, number)
+	var numbers []Number
+	var sum int
+	for i := 0; i < len(charArray); i++ {
+		number := ""
+		for j := 0; j < len(charArray[i]); j++ {
+			if unicode.IsDigit(charArray[i][j]) {
+				number += string(charArray[i][j])
+			} else {
+				x1, y1 := checkAdjacent(&charArray, i, j-len(number), j-1)
+
+				if x1 > 0 {
+					value, err := strconv.Atoi(number)
+					if err != nil {
+						fmt.Printf("Number to str fail: %s\n", number)
+					}
+					numbers = append(numbers, Number{x1, y1, value})
+				}
+				number = ""
+			}
+		}
+
+		// Check for a number at the end of the line
+		if number != "" {
+			x1, y1 := checkAdjacent(&charArray, i, len(charArray[i])-len(number), len(charArray[i])-1)
+			if x1 > 0 {
+				value, err := strconv.Atoi(number)
+				if err != nil {
+					fmt.Printf("Number to str fail: %s\n", number)
+				}
+				numbers = append(numbers, Number{x1, y1, value})
+			}
+			number = ""
 		}
 	}
 
-	if len(numbers) == 2 {
-		// Calculate product of the first two numbers found
-		return numbers[0] * numbers[1]
+	var filteredNumbers []Number
+
+	for _, number := range numbers {
+		if number.value != 0 {
+			filteredNumbers = append(filteredNumbers, number)
+		}
 	}
-	return 0
+
+	//for _, filteredNumber := range filteredNumbers {
+	//	fmt.Println(filteredNumber)
+	//}
+
+	groups := make(map[string][]Number)
+	for _, s := range filteredNumbers {
+		key := fmt.Sprintf("%d,%d", s.x, s.y) // Create a unique key for each coordinate pair
+		groups[key] = append(groups[key], s)
+	}
+
+	for _, numberList := range groups {
+		if len(numberList) == 2 {
+			fmt.Println(numberList)
+			sum += numberList[0].value * numberList[1].value
+		}
+	}
+
+	fmt.Println("Sum:", sum)
 }
 
-func extractNumber(grid [][]rune, x, y int, dir [2]int) int {
-	numberStr := ""
-	dx, dy := dir[1], dir[0]
+func checkAdjacent(array *[][]rune, i, start, end int) (int, int) {
 
-	for x >= 0 && x < len(grid[0]) && y >= 0 && y < len(grid) && unicode.IsDigit(grid[y][x]) {
-		numberStr += string(grid[y][x])
-		x += dx
-		y += dy
+	x, y := checkLeft(array, i, start)
+	if x > 0 {
+		return x, y
 	}
 
-	number, _ := strconv.Atoi(numberStr)
-	return number
+	x, y = checkRight(array, i, end)
+	if x > 0 {
+		return x, y
+	}
+
+	x, y = checkBottom(array, i, start, end)
+	if x > 0 {
+		return x, y
+	}
+
+	x, y = checkTop(array, i, start, end)
+	if x > 0 {
+		return x, y
+	}
+
+	x, y = checkDiagonalLeft(array, i, start)
+	if x > 0 {
+		return x, y
+	}
+
+	x, y = checkDiagonalRight(array, i, end)
+	if x > 0 {
+		return x, y
+	}
+
+	return -1, -1
+}
+
+func checkTop(array *[][]rune, i, start, end int) (int, int) {
+	if i > 0 {
+		for k := start; k <= end; k++ {
+			if (*array)[i-1][k] == '*' {
+				return i - 1, k
+			}
+		}
+	}
+	return -1, -1
+}
+
+func checkBottom(array *[][]rune, i, start, end int) (int, int) {
+	if i < len(*array)-1 {
+		for k := start; k <= end; k++ {
+			if (*array)[i+1][k] == '*' {
+				return i + 1, k
+			}
+		}
+	}
+	return -1, -1
+}
+
+func checkLeft(array *[][]rune, i, start int) (int, int) {
+	if start > 0 && (*array)[i][start-1] == '*' {
+		return i, start - 1
+	}
+	return -1, -1
+}
+
+func checkRight(array *[][]rune, i, end int) (int, int) {
+	if end < len((*array)[i])-1 && (*array)[i][end+1] == '*' {
+		return i, end + 1
+	}
+	return -1, -1
+}
+
+func checkDiagonalLeft(array *[][]rune, i, start int) (int, int) {
+	if i > 0 && start > 0 && (*array)[i-1][start-1] == '*' {
+		return i - 1, start - 1
+	}
+	if i < len(*array)-1 && start > 0 && (*array)[i+1][start-1] == '*' {
+		return i + 1, start - 1
+	}
+	return -1, -1
+}
+
+func checkDiagonalRight(array *[][]rune, i, end int) (int, int) {
+	if i > 0 && end < len((*array)[i])-1 && (*array)[i-1][end+1] == '*' {
+		return i - 1, end + 1
+	}
+	if i < len(*array)-1 && end < len((*array)[i])-1 && (*array)[i+1][end+1] == '*' {
+		return i + 1, end + 1
+	}
+	return -1, -1
 }
