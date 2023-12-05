7c27c014072fbecbf5f29b571f74496af08ad1cc
daronwastaken <zwierzchowski.kacper1@gmail.com>
2023-12-05 15:43:18 +0100

    Fix: day 4 part 2 optimization
    This time instead of appending games to the list, the number of repeats is increased each time a card is won. The result is a huge increase in performance

--- 4/4.go
+++ 4/4.go
@@ -12,8 +12,13 @@ import (
 
 const FilePath = "input.txt"
 
+/*
+Game For part 2, repeats number determines how
+many scratchcards will be awarded
+*/
 type Game struct {
 	id                 int
+	repeats            int
 	winningNumbers     []string
 	scratchcardNumbers []string
 }
@@ -33,6 +38,7 @@ func main() {
 
 	gameCount := 0
 
+	//part 1
 	for scanner.Scan() {
 
 		line := scanner.Text()
@@ -42,7 +48,8 @@ func main() {
 		winningNumbers := regexp.MustCompile(`\d+`).FindAllString(numbers[0], -1)
 		scratchcardNumbers := regexp.MustCompile(`\d+`).FindAllString(numbers[1], -1)
 
-		games = append(games, Game{id: gameCount, winningNumbers: winningNumbers, scratchcardNumbers: scratchcardNumbers})
+		//for part 2
+		games = append(games, Game{id: gameCount, repeats: 1, winningNumbers: winningNumbers, scratchcardNumbers: scratchcardNumbers})
 
 		gameCount++
 		count := 0
@@ -64,6 +71,9 @@ func main() {
 		sum += points
 	}
 
+	//part 2
+	scratchcards := 0
+
 	for i := 0; i < len(games); i++ {
 		count := 0
 		game := games[i]
@@ -75,14 +85,15 @@ func main() {
 
 		if count > 0 {
 			for n := 1; n <= count; n++ {
-				if n > len(games) {
+				if game.id+n > len(games) {
 					break
 				}
-				games = append(games, games[game.id+n])
+				games[game.id+n].repeats += game.repeats
 			}
 		}
+		scratchcards += game.repeats
 	}
 
 	fmt.Printf("Sum of points: %d\n", sum)
-	fmt.Printf("Number of scratchcards: %d\n", len(games))
+	fmt.Printf("Number of scratchcards: %d\n", scratchcards)
 }
